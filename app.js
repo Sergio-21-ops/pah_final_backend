290b5bf9fdbb04ce4dd60bf202e772ce6a2d15a5
Sergio-21-ops <83419882+Sergio-21-ops@users.noreply.github.com>
2026-02-08 22:31:30 -0300

    acut app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, '../.env') });
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
@@ -54,7 +54,10 @@ app.post('ruta', verificarIndefinido , (req,res) =>  {
 
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto http://localhost:3000 ');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+
 
