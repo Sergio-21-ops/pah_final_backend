7198e7e77f51327093c0db650dcd663b8ae429bb
Sergio-21-ops <83419882+Sergio-21-ops@users.noreply.github.com>
2026-02-08 22:09:24 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,32 +1,60 @@
 import express from "express";
+import path from 'path'
+import { fileURLToPath } from "url";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
+import dotenv  from "dotenv";
 import { artistasRutas, bandasRutas, discosRutas, usuariosRutas } from "./rutas/index.js";
+import cors from 'cors'; 
 
-dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin: "https://pah-final-frontend-6kue.vercel.app",
-    credentials: true
-}));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname,'public')));
 
-// Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGODB_URI)
-    .then(()=> console.log('Conexión a MongoDB Atlas correcta'))
-    .catch(err => console.log('Conexión incorrecta', err));
 
-// Rutas API
+.then(()=>console.log('Conexion correcta'))
+.catch((err)=>console.log('Conexion incorrecta',err));
+
+
+console.log(import.meta.url)
+console.log(__filename)
+console.log(__dirname)
+
 app.use('/discos', discosRutas);
 app.use('/usuarios', usuariosRutas);
 app.use('/artistas', artistasRutas);
 app.use('/bandas', bandasRutas);
 
-// Puerto dinámico para Render o 3000 local
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+
+function verificarIndefinido(req, res, next) {
+    if (req.method === "POST" || req.method === "PUT") {
+        if (Object.keys(req.body).length === 0) {
+            return res.status(400).json({ message: "Texto en JSON" });
+        }
+    }
+    next();
+}
+
+app.use(verificarIndefinido);
+app.post('ruta', verificarIndefinido , (req,res) =>  {
+   
+})
+
+
+
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:3000 ');
 });
+
